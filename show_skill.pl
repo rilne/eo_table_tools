6fa8cd1038736e3815c86123cd78077283cad026
rilne <rilne@users.noreply.github.com>
2020-09-09 22:48:22 -0400

    Added support for eox format currently requires changing $version in show_skill.pl (4 or X for now) group_by_usage.pl not yet updated added skill names for eox added some skill types for eox

--- show_skill.pl
+++ show_skill.pl
@@ -15,13 +15,40 @@ while(@ARGV)
 }
 $search_mode = 1 if not keys %type_targets;
 
-my $block_size = 376;
+my $version_format = {
+	"4"	=> {
+		"header_len"=> 24,
+		"block_size"=> 376,
+		"max_block"	=> 273,
+		"max_level"	=> 10,
+		"columns"	=> 8,
+		"zero_lvl"	=> 0,
+	},
+	"X"	=> {
+		"header_len"=> 36,
+		"block_size"=> 612,
+		"max_block"	=> 769,
+		"max_level"	=> 11,
+		"columns"	=> 12,
+		"zero_lvl"	=> 1,
+	},
+};
 
-my ($skills, $skill_notes) = load_list('skills.dat');
-my ($types,  $type_notes)  = load_list('types.dat');
+my $version = "X";
+
+
+
+my	($block_size, $header_len, $max_block, $max_level, $columns, $zero_lvl) =
+	@{$version_format->{$version}}
+	{"block_size","header_len","max_block","max_level","columns","zero_lvl"};
+
+my $sub_size = $max_level * 4 + 4;
+
+my ($skills, $skill_notes) = load_list("skills_${version}.dat");
+my ($types,  $type_notes)  = load_list("types_${version}.dat");
 
 if(keys %type_targets and not @targets)
-{ @targets = (0..273); }
+{ @targets = (0..$max_block); }
 #(length($table_blob)/ $block_size)
 
 
@@ -29,7 +56,7 @@ for my $block (@targets)
 {
 	my $block_blob = substr($table_blob, $block * $block_size, $block_size);
 	
-	my $block_header = substr($block_blob, 0, 24);
+	my $block_header = substr($block_blob, 0, $header_len);
 	
 	my $max_level = unpack("c", substr($block_header, 0, 1));
 	
@@ -43,16 +70,17 @@ for my $block (@targets)
 	
 	print $buffer " max level:".join(" ",
 		map {	($_ == $max_level)?("main"):
-				($_ == $max_level / 2)?(" sub"):("    ")} 1..10)." $max_level\n";
+				($_ == $max_level / 2)?(" sub"):
+				($_ == 0)?("   0"):("    ")} (!$zero_lvl)..$max_level)." $max_level\n";
 	
-	for my $sub (0..7)
+	for my $sub (0..($columns - 1))
 	{
-		my $sub_blob = substr($block_blob, 24 + $sub * 44, 44);
+		my $sub_blob = substr($block_blob, $header_len + $sub * $sub_size, $sub_size);
 		my ($type, @values) = unpack("i*", $sub_blob);
 		next unless $type;
 		next unless $search_mode or $type_targets{$type};
 		
-		print $buffer sprintf("%05X:%4d: ",$block * $block_size + 24 + $sub * 44, $type)
+		print $buffer sprintf("%05X:%4d: ",$block * $block_size + $header_len + $sub * $sub_size, $type)
 			.join(", ", map {sprintf("%3d", $_)} @values)." ".
 			($types->{$type} // "unknown")."\n";
 		
